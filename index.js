0503de06dd2bb93c9e58a9620a1820d43af7536e
BhavikJoshi <bhavik@getwalnut.com>
2019-01-29 17:58:51 +0530

    Fixed Positioning

--- index.js
+++ index.js
@@ -16,10 +16,23 @@ app.on('ready', () => {
   const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png';
   const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
   tray = new Tray(iconPath);
-  tray.on('click', () => {
+  tray.on('click', (event, bounds) => {
+    // Get Click Event Bounds
+    const { x, y } = bounds;
+
+    // Window Height and Width
+    const { height, width } = mainWindow.getBounds();
+
     if(mainWindow.isVisible()) {
       mainWindow.hide();
     } else {
+      const yPosition = process.platform === 'darwin' ? y : y - height;
+      mainWindow.setBounds({
+        x: x - width / 2,
+        y: yPosition,
+        height,
+        width
+      })
       mainWindow.show();
     }
   });
